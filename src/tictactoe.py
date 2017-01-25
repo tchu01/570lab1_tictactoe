5d45fe89c64b73d622f6d0e7ab768ef0fe8c5eee
Timothy Chu <tchu01@calpoly.edu>
2017-01-25 01:49:40 -0800

    Added initial attempt at alpha-beta

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -1,3 +1,6 @@
+import copy
+import math
+
 class TicTacToe:
     PLAYERX = 'X'
     PLAYERO = 'O'
@@ -25,20 +28,34 @@ class TicTacToe:
         self.game_over = False
 
     def print_board(self):
+        '''
+        Prints the current game state's board.
+        '''
+
         print(" " + self.board[0][0] + " | " + self.board[0][1] + " | " + self.board[0][2])
         print("-----------")
         print(" " + self.board[1][0] + " | " + self.board[1][1] + " | " + self.board[1][2])
         print("-----------")
         print(" " + self.board[2][0] + " | " + self.board[2][1] + " | " + self.board[2][2])
 
-    # returns GAMEOVER, PLACE_SUCCESS, and PLACE_FAIL
     def place(self, loc):
+        '''
+        Places the mark on the current game state's board.
+        If game resulted in victory, prints victor.
+        If game resulted in tie, prints tie.
+        In both games, self.game_over is set to True.
+
+
+        :param loc: the location to place the mark
+        :return: GAMEOVER | PLACE_SUCCESS | PLACE_FAIL
+        '''
+
         if loc in self.location and (self.location[loc] != self.PLAYERX or self.location[loc] == self.PLAYERO):
             board_loc = self.location[loc]
             self.board[board_loc[0]][board_loc[1]] = self.current_player
             self.remaining_moves -= 1
 
-            status = self.check_victory()
+            status = self.check_victory(self.remaining_moves, self.board)
             if status == self.VICTORY:
                 print()
                 self.print_board()
@@ -54,56 +71,129 @@ class TicTacToe:
                 self.game_over = True
                 return self.GAMEOVER
 
-            self.switch_player()
+            self.current_player = self.switch_player(self.current_player)
             return self.PLACE_SUCCESS
         else:
             print('Position is already taken or is invalid! ')
             print()
             return self.PLACE_FAIL
 
-    def switch_player(self):
-        if self.current_player == self.PLAYERX:
-            self.current_player = self.PLAYERO
+    def switch_player(self, player):
+        if player == self.PLAYERX:
+            return self.PLAYERO
         else:
-            self.current_player = self.PLAYERX
+            return self.PLAYERX
 
-    # returns TIE, VICTORY, and PLAY_ON
-    def check_victory(self):
-        if self.remaining_moves == 0:
-            return self.TIE
-        if self.check_victory_row() or self.check_victory_column() or self.check_victory_diagonal():
+    def check_victory(self, remaining_moves, board):
+        '''
+        Checks whether the passed in board resulted in a victory or tie.
+        Independent of current game state.
+
+        :param remaining_moves: remaining moves left on the passed in board
+        :param board: board for which victory will be checked
+        :return: TIE | VICTORY | PLAY_ON
+        '''
+
+        if self.check_victory_row(board) or self.check_victory_column(board) or self.check_victory_diagonal(board):
             return self.VICTORY
+        if remaining_moves == 0:
+            return self.TIE
         return self.PLAY_ON
 
-    def check_victory_row(self):
+    def check_victory_row(self, board):
         for i in range(3):
-            if self.board[i][0] == self.board[i][1] and self.board[i][0] == self.board[i][2]:
+            if board[i][0] == board[i][1] and board[i][0] == board[i][2]:
                 return True
 
         return False
 
-    def check_victory_column(self):
+    def check_victory_column(self, board):
         for i in range(3):
-            if self.board[0][i] == self.board[1][i] and self.board[0][i] == self.board[2][i]:
+            if board[0][i] == board[1][i] and board[0][i] == board[2][i]:
                 return True
 
         return False
 
-    def check_victory_diagonal(self):
-        if (self.board[0][0] == self.board[1][1] and self.board[0][0] == self.board[2][2]) or \
-                (self.board[0][2] == self.board[1][1] and self.board[0][2] == self.board[2][0]):
+    def check_victory_diagonal(self, board):
+        if (board[0][0] == board[1][1] and board[0][0] == board[2][2]) or \
+                (board[0][2] == board[1][1] and board[0][2] == board[2][0]):
             return True
 
         return False
 
-    def mini_max(self, level, current_player, alpha, beta):
-        # if current_player == PLAYERO
-            # find all available moves (children of current state)
 
-        # if current_player == PLAYERX
-            # find all available moves (children of current state)
 
-        print("Minimax alpha-beta")
+
+    def alpha_beta(self, board, current_player, alpha, beta):
+        possible_moves = self.possible_moves(board)
+
+        status = self.check_victory(len(possible_moves), board)
+        if status == self.VICTORY:
+            if current_player == self.PLAYERO:
+                return -10;
+            else:
+                return 10;
+        elif status == self.TIE:
+            return 0;
+
+        if current_player == self.PLAYERO:
+            # print("PLAYER0 making a move")
+            for move in possible_moves:
+                new_board = copy.deepcopy(board)
+                board_loc = self.location[move]
+                new_board[board_loc[0]][board_loc[1]] = current_player
+                # self.print_passed_board(new_board)
+                # print()
+
+                score = self.alpha_beta(new_board, self.switch_player(current_player), alpha, beta)
+                # print("score: " + str(score))
+                if score > alpha:
+                    # print("alpha = score")
+                    alpha = score
+                if alpha >= beta:
+                    # print("alpha >= beta")
+                    return alpha;
+
+            return alpha
+        else:
+            # print("PLAYERX making a move")
+            for move in possible_moves:
+                new_board = copy.deepcopy(board)
+                board_loc = self.location[move]
+                new_board[board_loc[0]][board_loc[1]] = current_player
+                # self.print_passed_board(new_board)
+                # print()
+
+                score = self.alpha_beta(new_board, self.switch_player(current_player), alpha, beta)
+                # print("score: " + str(score))
+                if score < beta:
+                    # print("beta = score")
+                    beta = score
+                if alpha >= beta:
+                    # print("alpha >= beta")
+                    return beta;
+
+            return beta
+
+    def possible_moves(self, board):
+        ret = []
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] is not self.PLAYERX and board[i][j] is not self.PLAYERO:
+                    ret.append(board[i][j])
+
+        return ret
+
+    def print_passed_board(self, board):
+        '''
+        Prints the passed in board.
+        '''
+
+        print(" " + board[0][0] + " | " + board[0][1] + " | " + board[0][2])
+        print("-----------")
+        print(" " + board[1][0] + " | " + board[1][1] + " | " + board[1][2])
+        print("-----------")
+        print(" " + board[2][0] + " | " + board[2][1] + " | " + board[2][2])
 
 
     def start_game(self):
@@ -125,8 +215,15 @@ class TicTacToe:
             self.print_board()
             print()
 
-            # self.mini_max()
+            inf = math.inf
+            print("ALPHA_BETA")
+            score = self.alpha_beta(self.board, self.current_player, -inf, inf)
+            print(score)
 
+            # self.place(score[1])
+            # print()
+            # self.print_board()
+            # print()
 
 
 if __name__ == "__main__":
